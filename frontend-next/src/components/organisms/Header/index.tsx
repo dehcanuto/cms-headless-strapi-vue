050e93e38daf6fcce7b28e7a10b1c5b4be982ad9
André Canuto <andrecanutoc@gmail.com>
2025-06-08 16:45:03 -0300

    feat: header with responsive done

---
+++ frontend-next/src/components/organisms/Header/index.tsx
@@ -0,0 +1,126 @@
+'use client';
+import { JSX, useState } from 'react';
+import Link from 'next/link';
+
+const Header = (): JSX.Element => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const adminUrl: string = `${process.env.NEXT_PUBLIC_API_URL}/admin`;
+
+  return (
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="text-2xl font-bold text-slate-700 -m-1.5 p-1.5">
+            André
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+              .can
+            </span>
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            onClick={() => setOpenMenu(true)}
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="size-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <Link href="/" className="font-semibold text-gray-900">
+            Posts
+          </Link>
+          <Link href="/" className="font-semibold text-gray-900">
+            Crie seu post!
+          </Link>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link href={adminUrl} className="font-semibold text-gray-900">
+            Log in
+          </Link>
+        </div>
+      </nav>
+
+      {/* Menu mobile visível só se openMenu for true */}
+      {openMenu && (
+        <div className="lg:hidden fixed inset-0 z-50 bg-white" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 z-40 bg-black/20" onClick={() => setOpenMenu(false)}></div>
+          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="text-2xl font-bold text-slate-700 -m-1.5 p-1.5">
+                André
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+                  .can
+                </span>
+              </a>
+              <button
+                onClick={() => setOpenMenu(false)}
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <svg
+                  className="size-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <Link
+                    href="/"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Posts
+                  </Link>
+                  <Link
+                    href="/"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Crie seu post!
+                  </Link>
+                </div>
+                <div className="py-6">
+                  <Link
+                    href={adminUrl}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
