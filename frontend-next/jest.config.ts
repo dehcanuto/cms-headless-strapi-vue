36f3199ba17535925eaa363de6a2fc4740b62c21
André Canuto <andrecanutoc@gmail.com>
2025-06-09 11:56:16 -0300

    feat: babel for jest

--- frontend-next/jest.config.ts
+++ frontend-next/jest.config.ts
@@ -1,11 +1,17 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'babel-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
+
+export default config;
